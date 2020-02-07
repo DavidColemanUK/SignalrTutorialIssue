aab6e2301c65e580da9f598d73a05befcb0922b0
David Coleman <David.Coleman@nottscc.gov.uk>
2020-02-07 15:28:30 +0000

    Add project files.

---
+++ SignalrTutorial/ChatHub.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace SignalrTutorial
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
+
